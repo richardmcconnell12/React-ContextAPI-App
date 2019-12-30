e84b51cec9ad8aa6a65e8513a695a319d9a0b0ca
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-12-30 17:12:45 -0600

    implemented on click auth -- multiple contexts

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import NavBar from "./components/NavBar";
 import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <NavBar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <NavBar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
