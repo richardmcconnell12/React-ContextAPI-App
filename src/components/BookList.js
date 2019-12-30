210a21bc1c5d026c82b219a279d0328283c247ae
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-12-30 16:03:52 -0600

    refactored navbar and booklist to use context.consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,19 +4,25 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class BookList extends Component {
   static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The Way of Kings</li>
-          <li style={{ background: theme.ui }}>The Name of the Wind</li>
-          <li style={{ background: theme.ui }}>The Final Empire</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The Way of Kings</li>
+                <li style={{ background: theme.ui }}>The Name of the Wind</li>
+                <li style={{ background: theme.ui }}>The Final Empire</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
